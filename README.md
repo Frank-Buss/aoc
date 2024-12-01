fd2fdff7f1b6f4e6c3210e45e845a871d1cf72bc
Frank Buss <fb@frank-buss.de>
2024-12-01 15:26:48 +0100

    initial commit with day 1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# aoc
+# Advent of Code
+Solutions for Advent of Code, with my puzzle data included. Run it like this:
+```
+cargo run
+```
+Output:
+```
+   Compiling aoc v0.1.0 (/home/frank/data/projects/aoc)
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.42s
+     Running `target/debug/aoc`
+solution 1: 1834060
+solution 2: 21607792
+```
---
+++ src/year2024/day1.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+pub fn solve(lines: Vec::<&str>) -> (String, String) {
+    // split each line and store it in 2 int vectors
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+    for line in lines {
+        let mut parts = line.split_whitespace();
+        left.push(parts.next().unwrap().parse::<i32>().unwrap());
+        right.push(parts.next().unwrap().parse::<i32>().unwrap());
+    }
+
+    // sort vectors in-place
+    left.sort();
+    right.sort();
+
+    // sum differences
+    let mut solution1 = 0;
+    for i in 0..left.len() {
+        solution1 += (left[i] - right[i]).abs();
+    }
+
+    // create histogram
+    let mut counts = HashMap::<i32, i32>::new();
+    for i in right {
+        *counts.entry(i).or_default() += 1;
+    }
+
+    // sum each left entry by number of occurences in right vector
+    let mut solution2 = 0;
+    for i in left {
+        solution2 += *counts.entry(i).or_default() * i;
+    }
+
+    // return solutions
+    (solution1.to_string(), solution2.to_string())
+}

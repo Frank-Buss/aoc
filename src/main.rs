5c8379419edd3c671645b71e4bc2124716b9c02d
Frank Buss <fb@frank-buss.de>
2024-12-05 08:20:37 +0100

    day 5

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ fn call_day(year: i32, day: i32) -> (i32, i32) {
         2 => year2024::day2::solve,
         3 => year2024::day3::solve,
         4 => year2024::day4::solve,
+        5 => year2024::day5::solve,
         _ => not_found,
     };
     fun(lines)
@@ -50,5 +51,6 @@ mod tests {
         assert_eq!(call_day(2024, 2), (510, 553));
         assert_eq!(call_day(2024, 3), (163931492, 76911921));
         assert_eq!(call_day(2024, 4), (2633, 1936));
+        assert_eq!(call_day(2024, 5), (2633, 1936));
     }
 }

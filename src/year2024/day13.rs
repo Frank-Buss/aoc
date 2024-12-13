94689a92b89d9416b2a9a657546e3bb70bc6b650
Frank Buss <fb@frank-buss.de>
2024-12-13 07:09:43 +0100

    day 13, part 1

--- src/year2024/day13.rs
+++ src/year2024/day13.rs
@@ -1,13 +1,36 @@
-pub fn solve(lines: Vec<String>) -> (String, String) {
-    let mut solution1: u64 = 0;
-    let mut solution2: u64 = 0;
+use regex::Regex;
 
-    for line in lines {
-        if line.len() > 0 {
-            solution1 += 1;
-            solution2 += 1;
+fn parse_pair(line: &String) -> (u32, u32) {
+    let bre = Regex::new(r".*?(\d+).+?(\d+)").unwrap();
+    let caps = bre.captures(line).unwrap();
+    (
+        caps[1].parse::<u32>().unwrap(),
+        caps[2].parse::<u32>().unwrap(),
+    )
+}
+
+pub fn solve(lines: Vec<String>) -> (String, String) {
+    let mut li = lines.iter();
+    let mut costs: Vec<u32> = Vec::new();
+    loop {
+        if let Some(line) = li.next() {
+            let (b1x, b1y) = parse_pair(line);
+            let (b2x, b2y) = parse_pair(li.next().unwrap());
+            let (px, py) = parse_pair(li.next().unwrap());
+            li.next();
+            for b1 in 0..=100 {
+                for b2 in 0..=100 {
+                    if b1x * b1 + b2x * b2 == px && b1y * b1 + b2y * b2 == py {
+                        costs.push(3 * b1 + b2);
+                    }
+                }
+            }
+        } else {
+            break;
         }
     }
 
-    (solution1.to_string(), solution2.to_string())
+    let solution1: u32 = costs.iter().sum();
+
+    (solution1.to_string(), "0".into())
 }

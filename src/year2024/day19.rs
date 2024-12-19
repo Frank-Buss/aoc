618a58a26ebab5c03ea5361a0ffb2cab385945c4
Frank Buss <fb@frank-buss.de>
2024-12-19 07:27:08 +0100

    day 19 part 1

--- src/year2024/day19.rs
+++ src/year2024/day19.rs
@@ -1,13 +1,55 @@
+use regex::Regex;
+use trie_rs::{Trie, TrieBuilder};
+
+use memoize::memoize;
+
+static mut TOWELS: Option<Trie<u8>> = None;
+
+#[memoize]
+fn search(pattern: String, start: usize) -> bool {
+    if start >= pattern.len() {
+        return true;
+    }
+    let mut found = false;
+    let results: Vec<String> = unsafe {
+        TOWELS
+            .as_ref()
+            .unwrap()
+            .common_prefix_search(pattern[start..].to_string())
+            .collect()
+    };
+    for s in results {
+        found |= search(pattern.clone(), start + s.len());
+    }
+    found
+}
+
 pub fn solve(lines: Vec<String>) -> (String, String) {
     let mut solution1: u64 = 0;
-    let mut solution2: u64 = 0;
 
-    for line in lines {
-        if line.len() > 0 {
-            solution1 += 1;
-            solution2 += 1;
+    let mut line_iter = lines.iter();
+    let line = line_iter.next().unwrap();
+    let r = r"(\w+)";
+    let r = Regex::new(&r).unwrap();
+    let mut towels = TrieBuilder::new();
+    for cap in r.find_iter(&line) {
+        towels.push(cap.as_str().to_string());
+    }
+    let towels = towels.build();
+    unsafe {
+        TOWELS = Some(towels);
+    }
+    line_iter.next();
+
+    loop {
+        if let Some(pattern) = line_iter.next() {
+            if search(pattern.clone(), 0) {
+                solution1 += 1;
+            }
+        } else {
+            break;
         }
     }
 
-    (solution1.to_string(), solution2.to_string())
+    (solution1.to_string(), "0".to_string())
 }

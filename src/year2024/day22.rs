5e595a7dc104fd751012cdc0597a4694b0a4423f
Frank Buss <fb@frank-buss.de>
2024-12-22 11:47:47 +0100

    removed puzzle input

--- src/year2024/day22.rs
+++ src/year2024/day22.rs
@@ -1,21 +1,16 @@
-#[derive(Debug, Clone, PartialEq, Hash, Eq)]
-struct Seller {
-    vs: Vec<u8>,
-    ds: Vec<Vec<i8>>,
-}
+use std::collections::HashMap;
 
 pub fn solve(lines: Vec<String>) -> (String, String) {
     let mut solution1: u64 = 0;
     let mut solution2: u64 = 0;
 
-    let mut sellers: Vec<Seller> = Vec::new();
+    let mut sellers: Vec<HashMap<Vec<i8>, u8>> = Vec::new();
     for line in lines {
         let mut vs: Vec<u8> = Vec::new();
         let mut ds: Vec<Vec<i8>> = Vec::new();
-        let mut last: u8 = 0;
         if line.len() > 0 {
             let mut v = line.parse::<u64>().unwrap();
-            last = (v % 10) as u8;
+            let mut last = (v % 10) as u8;
             let mut ds2: Vec<i8> = Vec::new();
             for _ in 0..2000 {
                 v = (v * 64 ^ v) % 16777216;
@@ -34,35 +29,34 @@ pub fn solve(lines: Vec<String>) -> (String, String) {
             }
             solution1 += v;
         }
-        sellers.push(Seller { vs, ds });
+        let mut v_by_ds: HashMap<Vec<i8>, u8> = HashMap::new();
+        for i in 0..vs.len() {
+            let ds = &ds[i];
+            if !v_by_ds.contains_key(ds) {
+                v_by_ds.insert(ds.clone(), vs[i]);
+            }
+        }
+        sellers.push(v_by_ds);
     }
 
-    let mut max = 0;
     for d1 in -9..=9 {
         for d2 in -9..=9 {
             for d3 in -9..=9 {
                 for d4 in -9..=9 {
-                    if d1 != 0 && d2 != 0 && d3 != 0 && d4 != 0 {
-                        let mut sum = 0;
-                        let ds = vec![d1, d2, d3, d4];
-                        for s in &sellers {
-                            let mut v = 0;
-                            for i in 0..s.vs.len() {
-                                if s.ds[i] == ds {
-                                    v = s.vs[i];
-                                    break;
-                                }
-                            }
-                            sum += v;
+                    let mut sum = 0 as u64;
+                    let ds = vec![d1, d2, d3, d4];
+                    for v_by_ds in &sellers {
+                        if v_by_ds.contains_key(&ds) {
+                            sum += *v_by_ds.get(&ds).unwrap() as u64;
                         }
-                        if sum > max {
-                            max = sum;
-                        }
+                    }
+                    if sum > solution2 {
+                        solution2 = sum;
                     }
                 }
             }
         }
     }
 
-    (solution1.to_string(), max.to_string())
+    (solution1.to_string(), solution2.to_string())
 }
